605dc07f82ac2eb728f6ccd8710929f7195a2b54
Peter Yakovlev <gorkiy@gmail.com>
2019-09-11 12:19:23 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thatnamegame2/precache-manifest.2cea99783cb5356ca70406359eb83578.js"
+  "/thatnamegame2/precache-manifest.8e49d32912fa1bff0e6799bac23d119c.js"
 );
 
 self.addEventListener('message', (event) => {
